4d20af52a999327a3881e5aa3b2b0d47d3e1bcb5
Amonsta <amana.ammi@gmail.com>
2020-07-20 13:21:20 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO bears(name, age,
+gender,color,temperament,alive)
+VALUES ('Mr. Chocolate',21,'male',
+'brown','sweet',1);
+
+INSERT INTO bears(name, age,
+gender,color,temperament,alive)
+VALUES ('Rowdy',28,'non-binary',
+'black and brown','cool',1);
+
+INSERT INTO bears(name, age,
+gender,color,temperament,alive)
+VALUES ('Tabitha',35,'male',
+'brown','angry',1);
+
+INSERT INTO bears(name, age,
+gender,color,temperament,alive)
+VALUES ('Sergeant Brown',30,'female',
+'black','hungry',1);
+
+INSERT INTO bears(name, age,
+gender,color,temperament,alive)
+VALUES ('Melissa',17,'female',
+'white','sweet',1);
+
+INSERT INTO bears(name, age,
+gender,color,temperament,alive)
+VALUES ('Grinch',98,'male',
+'white','mean',0);
+
+INSERT INTO bears(name, age,
+gender,color,temperament,alive)
+VALUES ('Wendy',34,'female',
+'white','nice',1);
+
+INSERT INTO bears(name, age,
+gender,color,temperament,alive)
+VALUES (null,15,'male',
+'blue','killer',1);
+
+
